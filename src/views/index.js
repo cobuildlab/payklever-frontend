4301937e243fba6d22ff268e958fe4b3ebfa0b42
Jose Villalobos <joseavv4@gmail.com>
2018-04-11 15:24:53 -0400

    removed unnecessary '/index' when importing from index.js files

--- src/views/index.js
+++ src/views/index.js
@@ -2,4 +2,4 @@ export { default as Login } from './login/login';
 export { default as Signup } from './signup/signup';
 export { default as AdminPages } from './admin-pages/admin-pages';
 export { default as ClientPages } from './client-pages/client-pages';
-export * from './components/index';
+export * from './components';
