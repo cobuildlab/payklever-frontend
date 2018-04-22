f7b19c4b103a201aa2b3635cb85c3d88d0134362
Jose Villalobos <joseavv4@gmail.com>
2018-04-22 16:02:11 -0400

    added GET fetch

--- src/fetch/fetch.services.js
+++ src/fetch/fetch.services.js
@@ -1,8 +1,15 @@
 import i18next from '../i18n/i18n';
+import { AuthStore } from '../stores';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-export function postData(url, data) {
+/**
+ * POST method fetch
+ * @param  {string}  url    Endpoint URL
+ * @param  {Boolean} isAuth true if api requires token
+ * @return {Promise}         the data from the endpoint
+ */
+export function postData(url, data, isAuth) {
   return new Promise((resolve, reject) => {
     checkConnection();
 
@@ -10,9 +17,34 @@ export function postData(url, data) {
         body: JSON.stringify(data),
         headers: {
           'Accept': 'application/json',
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          'Authorization': (isAuth) ? `Token ${AuthStore.getToken()}`: '',
         },
         method: 'POST',
+      })
+      .then(checkStatus)
+      .then((res) => resolve(res))
+      .catch((err) => reject(err));
+  });
+}
+
+/**
+ * GET method fetch
+ * @param  {string}  url    Endpoint URL
+ * @param  {Boolean} isAuth true if api requires token
+ * @return {Promise}         the data from the endpoint
+ */
+export function getData(url, isAuth) {
+  return new Promise((resolve, reject) => {
+    checkConnection();
+
+    return fetch(`${API_URL}${url}`, {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': (isAuth) ? `Token ${AuthStore.getToken()}`: '',
+        },
+        method: 'GET',
       })
       .then(checkStatus)
       .then((res) => resolve(res))
