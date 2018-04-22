31f383d8e5e0cfca06a2f2f197b0af0e6969ac51
Jose Villalobos <joseavv4@gmail.com>
2018-04-22 17:46:10 -0400

    removed client manager view

--- src/views/client-pages/client-manager/client-manager.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ClientManager from './client-manager';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<ClientManager />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
