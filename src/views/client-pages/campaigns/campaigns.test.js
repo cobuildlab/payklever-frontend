2bd6976d8c552a1751e032f2d8af675c153d9179
Jose Villalobos <joseavv4@gmail.com>
2018-04-24 03:17:27 -0400

    fix test router warning

--- src/views/client-pages/campaigns/campaigns.test.js
+++ src/views/client-pages/campaigns/campaigns.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Campaigns from './campaigns';
+import { BrowserRouter as Router } from "react-router-dom";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Campaigns />, div);
+  ReactDOM.render(<Router><Campaigns /></Router>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
